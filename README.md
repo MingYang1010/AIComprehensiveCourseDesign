6c2edee42c88c4373fd9cd095a74e71787b08978
Yan-Shaoming <75833712+Steven-Ysm@users.noreply.github.com>
2023-12-18 22:56:56 +0800

    code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Artificial-Intelligence-Comprehensive-Course-Design
+
+## Datasets
+[Caltech-UCSD Birds-200-2011](https://data.caltech.edu/records/20098), [Standford Cars](https://ai.stanford.edu/~jkrause/cars/car_dataset.html), [Standford Dogs](http://vision.stanford.edu/aditya86/ImageNetDogs/main.html), [*mini*ImageNet](https://arxiv.org/abs/1606.04080v2), [*tiered*ImageNet](https://arxiv.org/abs/1803.00676) and [WebCaricature](https://arxiv.org/abs/1703.03230) are available at [Google Drive](https://drive.google.com/drive/u/1/folders/1SEoARH5rADckI-_gZSQRkLclrunL-yb0) and [百度网盘(提取码：yr1w)](https://pan.baidu.com/s/1M3jFo2OI5GTOpytxgtO1qA).
+
+## Train
+
+在Config文件夹中放置了DeepBDC对于不同数据集的训练细节调整，在run_trainer.py中读取不同yaml即可。
+
+## Test
+
+在run_test.py中调整PATH，也就是训练后在results中生成的文件夹路径。
---
+++ run_test.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import sys
+
+sys.dont_write_bytecode = True
+
+import os
+import torch
+from core.config import Config
+from core import Test
+
+
+PATH = "./results/DeepBDC-WebCaricature-resnet12Bdc-5-1-Dec-15-2023-12-19-49"
+VAR_DICT = {
+    "test_epoch": 5,
+    "device_ids": "0",
+    "n_gpu": 1,
+    "test_episode": 600,
+    "episode_size": 2,
+}
+
+
+def main(rank, config):
+    test = Test(rank, config, PATH)
+    test.test_loop()
+
+
+if __name__ == "__main__":
+    config = Config(os.path.join(PATH, "config.yaml"), VAR_DICT).get_config_dict()
+
+    if config["n_gpu"] > 1:
+        os.environ["CUDA_VISIBLE_DEVICES"] = config["device_ids"]
+        torch.multiprocessing.spawn(main, nprocs=config["n_gpu"], args=(config,))
+    else:
+        main(0, config)
